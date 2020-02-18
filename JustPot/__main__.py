da73d7bb6b0b7ca40f5c44055db09bd7d6085cf2
Saransh <b18191@students.iitmandi.ac.in>
2020-02-18 15:14:43 +0530

    include ports and logfilepath

--- JustPot/__main__.py
+++ JustPot/__main__.py
@@ -4,16 +4,29 @@ JustPot <config_file>
 Simple TCP honeypot logger
 
 Usage:
-JustPot <config_filepath>
+JustPot --config FILE
 
 Options:
-<config_filepath>  Path to config options .init file
+--config FILE      Path to config options .ini file
 -h --help          show this screen
 """
+import configparser
+#config_filepath = 'etc/JustPot.ini'
+config_filepath = 'JustPot.ini'
 
-from docpot import docpot 
-args = docpot(__doc__)
+config = configparser.ConfigParser()
+config.read(config_filepath)
+ports = config.get('default','ports',raw = True,fallback = '22,88,443,8080,8888,9999,3306')
+log_filepath = config.get('default','logfile',raw = True,fallback = '/var/log/JustPot.log')
 
-print('Config file: %s',args['<config_filepaths>'])
+#ports log file path
+print('[*]Ports %s'%ports)
+print("[*]logfile %s"%logfile)
+
+try:
+    ports_list = ports.split(',')
+except Exception as e:
+    print('Error parsing ports: %s',ports)
 
 
+HoneyPot(ports_list,log_filepath)
